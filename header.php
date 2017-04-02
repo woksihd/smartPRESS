b41195f60cb80a654ceca30c6eb7ae340f520ff5
Jakab Krisztian <woksihd@gmail.com>
2017-04-02 12:50:03 +0200

    header stuff

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
         <head>
             <meta charset="UTF-8">
             <title><?php bloginfo( 'name' ); ?> - <?php the_title(); ?></title>
+            <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,500,700&amp;subset=latin-ext" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&amp;subset=latin-ext" rel="stylesheet">
             <?php wp_head(); ?>
         </head>
@@ -25,6 +26,29 @@
         ?>
         <body <?php body_class( $classes ); ?>>
             <section class="header">
+               
+                <nav class="navbar navbar-fixed-top shadow-bar">
+                    <div class="container-fluid"> 
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">smartPRESS</a>
+                        </div>
+                        
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar navbar-nav">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                
+                <div class="header-img">
+                    <div class="container">
+                        <div class="title">
+                            <h1> <?php bloginfo( 'name' ); ?> </h1>
+                            <p> <?php bloginfo( 'description' ); ?> </p>
+                        </div>
+                    </div>
+                </div>
                 
             </section>
             <section class="index">
