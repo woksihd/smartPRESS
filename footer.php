b6709376f08ec90a0bcbfb038ebd949a27c7726c
Jakab Krisztian <woksihd@gmail.com>
2017-04-02 20:46:03 +0200

    minicam subscribe form

--- footer.php
+++ footer.php
@@ -7,28 +7,29 @@
 ?>
         </section>
         <footer>
-            <script src="https://r3.minicrm.hu/api/minicrm.js?t=1490888498"></script>
             <section class="subscribe">
-                <h1>Subscribe for our newsletter!</h1>
-                <form class="inputgroup">
-                    <input type="hidden" FormHash="29638-0kdjt9vdu61bc8x3m0l3" action="https://r3.minicrm.hu/Api/Signup" method="post" class="TurnKeyCRM">
-                    <fieldset>
-                        <div class="InputBlock">
-                            <input  name="Contact[2165][Email]" id="Contact_Email_2165" language="HU" type="text" placeholder="Email..." />
-                        </div>
+                <div class="content text-center">
+                    <h1>The road is ahead of you!</h1>
+                    <p>Subscribe to our newsletter!</p>
+                    <script src="https://r3.minicrm.hu/api/minicrm.js?t=1490888498"></script>
+                    <form FormHash="29638-0kdjt9vdu61bc8x3m0l3" action="https://r3.minicrm.hu/Api/Signup" method="post" class="TurnKeyCRM">
                         
-                        <div class="InputBlock">
-                            <input  name="Contact[2165][FirstName]" id="Contact_FirstName_2165" language="HU" type="text" placeholder="Name..." />
-                        </div>
-            
-                        <div class="InputBlock hidden">
-                            <textarea name="ToDo[2167][Comment]" id="ToDo_Comment_2167" language="HU"></textarea>
-                        </div>
+                        <!-- hiddens -->
+                        <textarea class="hidden" name="ToDo[2167][Comment]" id="ToDo_Comment_2167" language="HU">FONTOS!</textarea>
+                        
+                        <!-- visible -->
+                        <input class="form-control" name="Contact[2165][Email]" id="Contact_Email_2165" language="HU" type="text" placeholder="Email..." />
+                        <input class="form-control" name="Contact[2165][FirstName]" id="Contact_FirstName_2165" language="HU" type="text" placeholder="Name..." />
+                        <input id="Submit_29638-0kdjt9vdu61bc8x3m0l3" type="Submit" value="Elküld">
+                        
+                        <!-- form respone -->
+                        <div id="Response_29638-0kdjt9vdu61bc8x3m0l3" style="display: none;" class="Response"></div>
+                   
+                    </form>
+                </div>
+            </section>
+            <section class="social">
                 
-                    </fieldset>
-                    <div id="Response_29638-0kdjt9vdu61bc8x3m0l3" style="display: none;" class="Response"></div>
-                    <input id="Submit_29638-0kdjt9vdu61bc8x3m0l3" type="Submit" value="Elküld">
-                </form>
             </section>
         </footer>
     </body>
